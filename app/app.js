57cbd3ffff6c66f6416efedced88e3881a059c5e
theChopix <kratkyjan244@gmail.com>
2023-05-29 00:28:58 +0200

    finished bachelor project

--- app/app.js
+++ app/app.js
@@ -162,4 +162,4 @@ app.post('/api/cb-native', function (req, res) {
     }
 });
 
-app.listen(3000, () => console.log('Decision tree visualization NodeJS app is running on port 3000'));
+app.listen(5000, () => console.log('Decision tree visualization NodeJS app is running on port 5000'));
